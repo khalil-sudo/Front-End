6fceafd80332c5a1963f7127b2bcad8aac7581a5
khalil-sudo <72109796+khalil-sudo@users.noreply.github.com>
2021-06-07 19:35:06 +0100

    added componets files to directory

---
+++ src/app/components/login/login.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+import {
+  FormGroup,
+  FormControl,
+  Validators,
+  FormBuilder,
+} from '@angular/forms';
+
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css'],
+})
+export class LoginComponent implements OnInit {
+  /*   loginForm = new FormGroup({
+    email: new FormControl(''),
+    password: new FormControl(''),
+  }); */
+  loginForm = this.fb.group({
+    email: ['', Validators.required],
+    password: ['', Validators.required],
+  });
+  constructor(private fb: FormBuilder, private rout: Router) {}
+
+  ngOnInit(): void {}
+
+  onSubmit() {
+    console.warn(this.loginForm.value);
+
+    if (
+      this.loginForm.value.email == 'test@test.com' &&
+      this.loginForm.value.password == 'pass'
+    ) {
+      this.rout.navigate(['/home']);
+    } else alert('wrong email or password');
+  }
+}
