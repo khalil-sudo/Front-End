f236f7db3bbc05c49f9bbe772bc6e868cdd6cc0e
khalil-sudo <72109796+khalil-sudo@users.noreply.github.com>
2021-06-07 20:23:29 +0100

    repaired false imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,18 +13,31 @@ import { AddEditSujComponent } from './components/sujets/add-edit-suj/add-edit-s
 import { ShowSujComponent } from './components/sujets/show-suj/show-suj.component';
 import { EncadrantsComponent } from './components/encadrants/encadrants.component';
 import { AddEditEncComponent } from './components/encadrants/add-edit-enc/add-edit-enc.component';
-import { ShowEncComponent } from './components/show-enc/show-enc.component';
 import { StagiairesComponent } from './components/stagiaires/stagiaires.component';
 import { AddEditStaComponent } from './components/stagiaires/add-edit-sta/add-edit-sta.component';
 import { ShowStaComponent } from './components/stagiaires/show-sta/show-sta.component';
 import { AddEditStgComponent } from './components/stages/add-edit-stg/add-edit-stg.component';
-import { ShowStageComponent } from './components/stages/show-stage/show-stage.component';
 import { StagesComponent } from './components/stages/stages.component';
 import { ShowStgComponent } from './components/stages/show-stg/show-stg.component';
 import { HomepageComponent } from './components/homepage/homepage.component';
 
 @NgModule({
-  declarations: [AppComponent, LoginComponent, SujetsComponent, AddEditSujComponent, ShowSujComponent, EncadrantsComponent, AddEditEncComponent, ShowEncComponent, StagiairesComponent, AddEditStaComponent, ShowStaComponent, AddEditStgComponent, ShowStageComponent, StagesComponent, ShowStgComponent, HomepageComponent],
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    SujetsComponent,
+    AddEditSujComponent,
+    ShowSujComponent,
+    EncadrantsComponent,
+    AddEditEncComponent,
+    StagiairesComponent,
+    AddEditStaComponent,
+    ShowStaComponent,
+    AddEditStgComponent,
+    StagesComponent,
+    ShowStgComponent,
+    HomepageComponent,
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
